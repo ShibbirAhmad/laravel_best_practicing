f99d7c52590d3f0229e07ffc47b002ed91a1713f
ShibbirAhmad <shibbirahmad920229@gmail.com>
2020-03-14 14:55:07 +0600

    application is ready with admin template

---
+++ resources/views/admin/layout/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('admin/master')
+
+@section('title','welcome Dashboard')
+  
+
+@section('content')
+
+<div id="content-wrapper">
+
+  <div class="container-fluid">
+
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="index.blade.php">Dashboard</a>
+      </li>
+      <li class="breadcrumb-item active">Blank Page</li>
+    </ol>
+    <!-- Page Content -->
+    <h1>Blank Page</h1>
+    <hr>
+    <p>This is a great starting point for new custom pages.</p>
+
+
+</div>
+    
+@endsection
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,74 @@
+<div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.blade.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <h6 class="dropdown-header">Login Screens:</h6>
+            <a class="dropdown-item" href="login.blade.php">Login</a>  
+            <a class="dropdown-item" href="register.blade.php">Register</a>
+            <a class="dropdown-item" href="forgot-password.blade.php">Forgot Password</a>
+            <div class="dropdown-divider"></div>
+            <h6 class="dropdown-header">Other Pages:</h6>
+            <a class="dropdown-item" href="404.blade.php">404 Page</a>
+            <a class="dropdown-item active" href="blank.blade.php">Blank Page</a>
+          </div>
+        </li>
+     
+     
+        <li class="nav-item dropdown ">
+            <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             <i class="fas fa-fw fa-folder"></i>
+
+                   <span>Post Option<span>
+            </a>    
+            
+            <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                <h6 class="dropdown-header">PostsLogin Screens:</h6>
+                <a class="dropdown-item" href="login.blade.php">Add Post</a>  
+                <a class="dropdown-item" href="register.blade.php">PostList</a>
+                
+            </div>
+            
+        </li>
+
+        <li class="nav-item dropdown ">
+            <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+             <i class="fas fa-fw fa-folder"></i>
+
+                   <span>Category Option<span>
+            </a>    
+            
+            <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+                <h6 class="dropdown-header">Category Screens:</h6>
+                <a class="dropdown-item" href="login.blade.php">Add Category</a>  
+                <a class="dropdown-item" href="register.blade.php">Category List</a>
+                
+            </div>
+            
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="charts.blade.php">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tables.blade.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+        </li>
+      </ul>
+  
+     
